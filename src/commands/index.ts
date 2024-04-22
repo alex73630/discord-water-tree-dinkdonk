7c5a9e87336d81cf3730b3d78788632d50b2f1c9
Alexandre Sanchez <contact@alexandresanchez.xyz>
2024-04-22 14:58:23 +0200

    feat: enable stats command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,7 @@
 import { SetTreeCommand } from "~/commands/utility/set-tree.command"
+import { StatsCommand } from "~/commands/utility/stats.command"
 import { type BaseCommand } from "~/interfaces/base-command.interface"
 
-const commands: BaseCommand[] = [new SetTreeCommand()]
+const commands: BaseCommand[] = [new SetTreeCommand(), new StatsCommand()]
 
 export default commands
